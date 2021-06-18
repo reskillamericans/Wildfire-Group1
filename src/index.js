e57aac321fde1e1b0ee0668185c693eee0c50098
Oluwaseyi Ogunjuyigbe <seyiogunjuyigbe@gmail.com>
2021-06-18 10:03:36 +0100

    git reset

---
+++ src/index.js
@@ -0,0 +1,48 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const faqRouter = require('./routes/faqRoutes');
+const port = process.env.PORT || 3000;
+
+//Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//See console.log of whether you are in production or development mode
+console.log(process.env.NODE_ENV);
+
+//==================================================
+// DATABASE
+//==================================================
+const dbSetup = require('./database/setup');
+
+dbSetup();
+
+//==================================================
+// Routes
+//==================================================
+
+//FAQ Routes
+app.use('/faqs', faqRouter);
+
+//Placeholder routes for webpages
+app.get('/', (req, res) => {
+  res.send('Wildfire Group-1 Landing page');
+});
+
+app.get('/contact-us', (req, res) => {
+  res.send('Contact Page!');
+});
+
+app.get('/faq', (req, res) => {
+  res.send('FAQ');
+});
+
+app.get('/about-us', (req, res) => {
+  res.send('About Us');
+});
+
+//Server
+app.listen(port, () => {
+  console.log(`Server is listening on port: ${port}`);
+});

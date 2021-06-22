830496ce5fecb13383c01b8a624e459087bae62f
Emmy Le <emilytuyetle@gmail.com>
2021-06-22 13:19:34 -0700

    about us update

--- src/routes/memberRoutes.js
+++ src/routes/memberRoutes.js
@@ -2,6 +2,5 @@ const express = require('express');
 const router = express.Router();
 const MemberCtrl =require('../controllers/memberControllers');
 
-router.post('/members',MemberCtrl.createNewMember);
 router.get('/members',MemberCtrl.fetchMembers);
 router.get('/members', MemberCtrl.fetchSingleMember);
